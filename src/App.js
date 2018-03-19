92e3df7657bcd04daa0fd2eb8ebcc6a8bb316612
Miss Pylons <megmrob@gmail.com>
2018-03-19 23:41:40 +0000

    moved items

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-import keys from 'config.js';
+import api_key from 'config.js';
 import './App.css';
 import Form from './components/Form';
 import Nav from './components/Nav';
@@ -21,7 +21,7 @@ class App extends Component {
   }
   
   performSearch = (query = 'cats') => {
-    Axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${keys.key}&text=${query}&per_page=9&page=1&format=json&nojsoncallback=1`)
+    Axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${api_key}&text=${query}&per_page=9&page=1&format=json&nojsoncallback=1`)
     .then(response => {
       console.log(response);
     })
